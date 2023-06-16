95ef48bf376a40702aefd257207d4d99445cf799
Hamaad Saab <126661322+hamaad531@users.noreply.github.com>
2023-06-16 11:00:08 -0700

    Add files via upload
    sort problem

---
+++ sort.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+using namespace std;   
+int main() 
+{ 
+int array[] = {10, 35, 85, 93, 62, 77, 345, 43, 2, 10}; 
+int n = sizeof(array)/sizeof(array[0]); // size of array   
+// 'sizeof' gives the size of total array i.e. size of each character * no. of characters
+// so to get no. of characters
+// we divide the sizeof(array) with the size of any one character of the array
+// here it is array[0]
+sort(array, array+n);   
+cout << "nArray after sorting using default sort is : n"; 
+for (int i = 0; i < n; ++i) 
+cout << array[i] << " ";   
+return 0; 
+}
