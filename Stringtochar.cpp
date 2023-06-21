7063070f4fec3285ebb1607565890d7c8ccbd662
Hamaad Saab <126661322+hamaad531@users.noreply.github.com>
2023-06-21 10:42:49 -0700

    Add files via upload
    string to char

---
+++ Stringtochar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    string s = " zaidamjadghafarahmaads";
+    for (int i = 0; i < s.length(); i++)
+    {
+        char a = s[i];
+        cout << a << endl;
+    }
+    // in chracter we only initiliaze one charater
+
+    // cout << s << endl;
+    return 0;
+}
